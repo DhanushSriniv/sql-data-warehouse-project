e64329f529cae5fd19a0f35243ecad348e8da396
DhanushSriniv <96661903+DhanushSriniv@users.noreply.github.com>
2026-02-01 13:20:37 -0500

    Analytics Database Intiated
    New database been initiated for to perform basic EDA and advanced Data Analytics

---
+++ scripts/data_analytics/01_init_database.sql
@@ -0,0 +1,110 @@
+--- Data Warehouse: Basic EDA Approach 
+
+-- Creating a new database by exporting the gold layer as a new database
+
+-- Creating new Database
+USE master;
+GO
+
+
+-- Drop and recreate the 'DataWarehouseAnalytics' database
+IF EXISTS (Select 1 FROM sys.databases WHERE name = 'DataWarehosueAnalytics')
+BEGIN 
+	  ALTER DATABASE DataWarehosueAnalytics SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	  DROP DATABASE DataWarehosueAnalytics;
+END;
+GO
+
+-- Create the 'DataWarehosueAnalytics' database
+CREATE DATABASE DataWarehosueAnalytics;
+GO
+
+USE DataWarehosueAnalytics;
+GO
+
+-- Create Schemas
+
+CREATE SCHEMA gold;
+GO
+
+CREATE TABLE gold.dim_customers(
+	customer_key nvarchar(50),
+	customer_id nvarchar(50),
+	customer_number nvarchar(50),
+	first_name nvarchar(50),
+	last_name nvarchar(50),
+	country nvarchar(50),
+	marital_status nvarchar(50),
+	gender nvarchar(50),
+	birthdate nvarchar(50),
+	create_date nvarchar(50)
+);
+GO
+
+CREATE TABLE gold.dim_products(
+	product_key nvarchar(50) ,
+	product_id nvarchar(50) ,
+	product_number nvarchar(50) ,
+	product_name nvarchar(50) ,
+	category_id nvarchar(50) ,
+	category nvarchar(50) ,
+	subcategory nvarchar(50) ,
+	maintenance nvarchar(50) ,
+	cost nvarchar(50),
+	product_line nvarchar(50),
+	start_date nvarchar(50) 
+);
+GO
+
+CREATE TABLE gold.fact_sales(
+	order_number nvarchar(50),
+	product_key nvarchar(50),
+	customer_key nvarchar(50),
+	order_date nvarchar(50),
+	shipping_date nvarchar(50),
+	due_date nvarchar(50),
+	sales_amount nvarchar(50),
+	quantity nvarchar(50),
+	price nvarchar(50) 
+);
+GO
+
+TRUNCATE TABLE gold.dim_customers;
+GO
+
+BULK INSERT gold.dim_customers
+FROM 'C:\Users\Administrator\OneDrive\Desktop\Portfolio Building\Data Engineer\Projects\1. SQL Warehouse Project\2. GitHub Repository\sql-data-warehouse-project\sql-data-warehouse-project\gold_layer_datasets\gold.dim_customer.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
+GO
+
+
+TRUNCATE TABLE gold.dim_products;
+GO
+
+BULK INSERT gold.dim_products
+FROM 'C:\Users\Administrator\OneDrive\Desktop\Portfolio Building\Data Engineer\Projects\1. SQL Warehouse Project\2. GitHub Repository\sql-data-warehouse-project\sql-data-warehouse-project\gold_layer_datasets\gold.dim_products.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
+GO
+
+TRUNCATE TABLE gold.fact_sales;
+GO
+
+BULK INSERT gold.fact_sales
+FROM 'C:\Users\Administrator\OneDrive\Desktop\Portfolio Building\Data Engineer\Projects\1. SQL Warehouse Project\2. GitHub Repository\sql-data-warehouse-project\sql-data-warehouse-project\gold_layer_datasets\gold.fact_sales.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
+GO
+
+
+
